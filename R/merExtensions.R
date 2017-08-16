7d524f06453df0dec76c0f77e376277d0c151521
Jason Grafmiller <jason.grafmiller@gmail.com>
2017-08-16 10:46:28 +0100

    Update merExtensions.R
    Cleaned code, removed outdated functions, and added a few function descriptions

--- R/merExtensions.R
+++ R/merExtensions.R
@@ -1,55 +1,10 @@
 #############################################################################
-#
 # Adapted functions for working with lme4 merMod objects
 # v 0.1
 #
 # Jason Grafmiller
 # Aug 04, 2015
 #############################################################################
-
-aic.varimp <- function(fit, random = NULL){
-	# function for calculating the differences in AIC with predictors removed
-	require(MuMIn, quietly = T) # add MuMIn for calculating conditional AIC
-	if (class(fit) == "lmerMod" || class(fit) == "glmerMod"){
-		vars <- colnames(attr(attr(fit@frame, "terms"), "factors"))
-		# only fixed effs
-		# vars <- strsplit(toString(attr(attr(fit@frame, "terms"),
-		#															 "predvars.fixed")), ', ')[[1]][-c(1:2)]
-	}
-	else vars <- colnames(attr(attr(fit$model, "terms"), "factors"))
-
-	full.aic <- AICc(fit)
-	mod.aic <- c()
-
-	cat("variables run:\n")
-
-	# loop through (fixed effects) predictors
-	for (i in seq(1, length(vars))){
-		# find main effect and any interactions
-		cur_vars = paste(vars[which(grepl(vars[i], vars))], collapse = ' - ')
-		new.formula = reformulate(termlabels = paste(". -", cur_vars, ""),
-															response = ".")
-		mod.aic <- c(mod.aic,
-								 AICc(update(fit, new.formula)))
-		cat(cur_vars, ", ", sep = "")
-	}
-
-	# add AIC values to dataframe
-	aic.vals <- c(full.aic, mod.aic)
-	aic.diffs <- aic.vals - full.aic
-	aic.data <- data.frame(Predictor = c("Full model", vars),
-												 AIC = aic.vals,
-												 AICdiff = aic.diffs
-	)
-	aic.data <- droplevels(subset(aic.data, AICdiff != 0))
-	# sort the dataframe
-	aic.data <- aic.data[order(aic.data$AICdiff, decreasing = T),]
-	# order the factor levels (for ggplot2)
-	aic.data$Predictor <- factor(aic.data$Predictor,
-															 levels = rev(aic.data$Predictor))
-	return (aic.data)
-}
-
 
 colldiag.mer <- function (fit, scale = TRUE, center = FALSE, add.intercept = TRUE) {
 	## adapted from perturb::colldiag, method in Belsley, Kuh, and
@@ -58,19 +13,16 @@ colldiag.mer <- function (fit, scale = TRUE, center = FALSE, add.intercept = TRU
 	## more than one high variance propotion.  see ?colldiag for more
 	## tips.
 	result = NULL
-	if (center)
-		add.intercept = FALSE
+	if (center) add.intercept = FALSE
 	if (is.matrix(fit) || is.data.frame(fit)) {
 		X = as.matrix(fit)
 		nms = colnames(fit)
-	}
-	else if (class(fit)[1] %in% c("glmerMod", "lmerMod")) {
+	} else if (class(fit)[1] %in% c("glmerMod", "lmerMod")) {
 		nms = names(fixef(fit))
 		X = getME(fit, "X")
 		if (any(grepl("(Intercept)", nms))) {
 			add.intercept = FALSE
-		}
-	}
+		}}
 	X = X[!is.na(apply(X, 1, all)), ]
 
 	if (add.intercept) {
@@ -95,29 +47,23 @@ colldiag.mer <- function (fit, scale = TRUE, center = FALSE, add.intercept = TRU
 	} else {
 		rownames(condindx) = 1:length(condindx)
 		colnames(pi) = 1:ncol(pi)
-		rownames(pi) = 1:nrow(pi)
-	}
-
+		rownames(pi) = 1:nrow(pi)}
 	result = data.frame(cbind(condindx, pi))
 	zapsmall(result)
-}
+	}}
 
 
 collin.fnc.mer <- function(fit){
-	# adaption of Baayen's collin.fnc() for compatibility with current version
+	# adaption of languageR::collin.fnc() for compatibility with current version
 	# of lme4
 	require(languageR, quietly = T)
 	if (class(fit) == "lmerMod" || class(fit) == "glmerMod"){
 		return (collin.fnc(getME(fit, "X")[,-1]))
-	}
-	else stop("model not a merMod object")
+	} else stop("model not a merMod object")
 }
 
 
-kappa.mer <- function (fit,
-											 scale = TRUE, center = FALSE,
-											 add.intercept = TRUE,
-											 exact = FALSE) {
+kappa.mer <- function (fit, scale = TRUE, center = FALSE, add.intercept = TRUE, exact = FALSE) {
 	# adapted version of base R's kappa()
 	X = getME(fit,"X")
 	nam = names(fixef(fit))
@@ -125,8 +71,7 @@ kappa.mer <- function (fit,
 	nrp = sum(1 * (nam == "(Intercept)"))
 	if (nrp > 0) {
 		X = X[, -(1:nrp), drop = FALSE]
-		nam = nam[-(1:nrp)]
-	}
+		nam = nam[-(1:nrp)]	}
 	if (add.intercept) {
 		X = cbind(rep(1), scale(X, scale = scale, center = center))
 		kappa(X, exact = exact)
@@ -145,16 +90,13 @@ maxcorr.mer <- function (fit, exclude.intercept = TRUE) {
 	ns = sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
 	if (ns > 0 & exclude.intercept) {
 		corF = corF[-(1:ns), -(1:ns), drop = FALSE]
-		nam = nam[-(1:ns)]
-	}
+		nam = nam[-(1:ns)]}
 	corF[!lower.tri(corF)] = 0
 	maxCor = max(corF)
 	minCor = min(corF)
 	if (abs(maxCor) > abs(minCor)) {
 		zapsmall(maxCor)
-	} else {
-		zapsmall(minCor)
-	}
+	} else zapsmall(minCor)
 }
 
 
@@ -164,8 +106,7 @@ overdisp.mer <- function(model) {
 	##   an n-by-n variance-covariance matrix
 	# from http://glmm.wikidot.com/faq
 	vpars <- function(m) {
-		nrow(m)*(nrow(m) + 1)/2
-	}
+		nrow(m)*(nrow(m) + 1)/2}
 	model.df <- sum(sapply(VarCorr(model), vpars)) + length(fixef(model))
 	rdf <- nrow(model.frame(model)) - model.df
 	rp <- residuals(model, type = "pearson")
@@ -177,6 +118,15 @@ overdisp.mer <- function(model) {
 
 
 permute.varimp <- function(fit, data, verbose = F){
+	# Function for computing permutation based measures of predictor importance in regression models
+	# similar in spirit to method used by party::varimp() and party::varimpAUC().
+	# Loops through each predictor in the model (including random effects groups) and randomly 
+	# permutes its values. Then the model is refitted to the dataframe with the permuted predictor and
+	# various model statistics---C, AICc, and predictive accuracy---and these are compared to those
+	# of the original model. The output dataframe represents the difference between the original model
+	# and the model with the permuted predictor. Larger differences reflect greater importance.
+	# I prefer this method to the "drop-one" method because the model structures (and dfs) are exactly
+	# the same, and we can include both fixed and random effects. 
   require(lme4, quietly = T)
   require(Hmisc, quietly = T)
   require(MuMIn, quietly = T)
@@ -186,14 +136,11 @@ permute.varimp <- function(fit, data, verbose = F){
     vars <- colnames(attr(attr(fit@frame, "terms"), "factors"))
     y <- fit@resp$y
     # only fixed effs
-    # vars <- strsplit(toString(attr(attr(fit@frame, "terms"),
-    #															 "predvars.fixed")), ', ')[[1]][-c(1:2)]
-  }
-  else if (class(fit)[1] == "lrm"){
+    # vars <- strsplit(toString(attr(attr(fit@frame, "terms"), "predvars.fixed")), ', ')[[1]][-c(1:2)]
+  } else if (class(fit)[1] == "lrm"){
     vars <- colnames(attr(f.lrm$terms, "factors"))
     y <- as.numeric(fit$y) - 1
-  }
-  else {
+  } else {
     vars <- colnames(attr(attr(fit$model, "terms"), "factors"))
     y <- fit$y
   }
@@ -217,8 +164,7 @@ permute.varimp <- function(fit, data, verbose = F){
     new.probs <- predict(new_fit, type = "response")
     if (class(fit) == "lmerMod" || class(fit) == "glmerMod"){
       new_y <- new_fit@resp$y
-    }
-    else {
+    } else {
       new_y <- new_fit$y
     }
     new.C <- somers2(new.probs, new_y)[[1]]
@@ -252,30 +198,62 @@ ranef.disp <- function(fit){
 					ggtitle(paste0(lab, ":", cols[j]))
 				plist <- append(plist, list(p))
 			}
-		}
-		else {
+		} else {
 			p <- ggQQ.plot(d, cols) + ggtitle(lab)
 			plist <- append(plist, list(p))
 			print(i)
-		}
-	}
+		}}
 	do.call("grid.arrange", c(plist, ncol = 2))
 }
 
 
-scores.mer <- function(fit){
-  probs <- predict(fit, type = "response")
-  y <- getME(fit, "y")
-  # AUC and Dxy
-  C <- Hmisc::somers2(probs, y)[1]
-  Dxy <- Hmisc::somers2(probs, y)[2]
-  # log-loss (- average log-likelihood)
-  LL <- - mean(y*log(d$probs) + (1 - y)*log(1 - d$probs))
-  # accuracies
-  resp <- levels(gen_glmm@frame[,1])
-  d.1 <- subset(d, Response == resp[1])
-  d.2 <- subset(d, Response == resp[2])
-
+scores.mer <- function(fit, R2 = F, digits = 3){
+	# Calculates a number of evaluative statistics for binomial mixed effects regression models fit with glmer().
+	# - c, Dxy, AICc, Log likelihood, kappa, log loss (cross entropy loss), accuracy and average per class 
+	# accuracy. Optionally calculates marginal and conditional (pseudo) R squared
+	require(dplyr)
+	require(magrittr)
+	require(Hmisc)
+	require(MuMIn)
+	d <- fit@frame # dataframe
+ 	probs <- predict(fit, type = "response")
+  	preds <- ifelse(probs > .5, 1, 0)
+  	y <- getME(fit, "y")
+  	d2 <- cbind(d, data.frame(probs = probs, preds = preds, y = y))
+  	# AUC and Dxy
+  	C <- Hmisc::somers2(probs, y)[1] %>% as.vector
+  	Dxy <- Hmisc::somers2(probs, y)[2] %>% as.vector
+ 	AICc <- MuMIn::AICc(fit)
+  	logL <- logLik(fit)[1]
+  	N <- nrow(d)
+  	kappa <- collin.fnc.mer(fit)$cnumber # measure of multicollinearity
+  	# log-loss (- average log-likelihood)
+  	LL <- - mean(y*log(probs) + (1 - y)*log(1 - probs))
+  	# predictive accuracies
+  	acc <- length(which(preds == y))/nrow(d)
+  	base <- 1 - mean(y)
+  	resp <- levels(d[, 1])
+  	resp_col <- names(d)[1]
+  	d.1 <- dplyr::filter_(d2, paste0(resp_col," == '", resp[1],"'")) %>% droplevels
+  	d.2 <- dplyr::filter_(d2, paste0(resp_col," == '", resp[2],"'")) %>% droplevels
+		class1.acc <- length(which(d.1$preds == d.1$y))/nrow(d.1)
+		class2.acc <- length(which(d.2$preds == d.2$y))/nrow(d.2)
+		out <- c(N = N, C = C, Dxy = Dxy, AICc = AICc, kappa = kappa,
+			predicted.corr = acc,
+			baseline = base,
+			Log.Loss = LL,
+			Class.1.acc = class1.acc,
+			Class.2.acc = class2.acc,
+			Avg.Per.Class = mean(c(class1.acc, class2.acc)))
+	if (R2){
+		R2 <- MuMIn::r.squaredGLMM(fit)
+		out <- c(out, R2.m = R2[[1]], R2.c = R2[[2]])
+	}
+	if (is.null(digits)){
+		return (out)
+	} else {
+		return (round(out, digits))
+	}
 }
 
 
@@ -288,58 +266,6 @@ somers.mer <- function(fit, ...){
 		return (somers2(fitted(fit), as.numeric(y), ...))
 	}
 	else stop("object not of class glmerMod")
-}
-
-
-sum.stats.mer <- function(fit, y = NULL, rnd = T, R2 = T){
-	# function for creating a list object with summary statistics for
-	# mixed-effects models.
-	require(Hmisc, quietly = TRUE)
-	require(MuMIn, quietly = TRUE)
-	response <- getME(fit,"y")
-	z <- attr(fit@frame, "terms")
-	depvar <- names(attr(terms(z), "dataClasses")[attr(terms(z),"response")])
-	outcomes <- levels(fit@frame[,1])
-	probs <- fitted(fit)
-	DF <- attr(logLik(fit),"df")
-	N <- length(fit@resp$n)
-	logL <- logLik(fit)[1]
-	AIC <- AIC(fit)
-	AIC.c <- AIC(fit) + ((DF+1)*2*DF)/(N-DF-1)
-	C <- somers2(probs, response)[1]
-	Dxy <- somers2(probs, response)[2]
-	percent.corr <- sum(ifelse(probs>.5,1,0)==response)/N
-	baseline.acc <- max(table(response))/N
-	k <- kappa(cbind(1, scale(getME(fit,"X")[,-1],center=F)), exact=T)
-	R2 <- r.squaredGLMM(fit)
-	# computing R2 takes some time, so leave it as optional
-	if(R2){
-		R2.m <- R2[[1]]
-		R2.c <- R2[[2]]
-		stats = list(N = as.integer(N), df = as.integer(DF),
-								 logLik = logL,
-								 AIC = AIC,
-								 AICc = AIC.c,
-								 C = C,
-								 Dxy = Dxy,
-								 percent.corr = (100*percent.corr),
-								 baseline.acc = (100*baseline.acc),
-								 kappa = k,
-								 R2.m = R2.m,
-								 R2.c = R2.c)
-	}
-	else{
-		stats = list(N = as.integer(N), df = as.integer(DF),
-								 logLik = logL,
-								 AIC = AIC,
-								 AICc = AIC.c,
-								 C = C,
-								 Dxy = Dxy,
-								 percent.corr = (100*percent.corr),
-								 baseline.acc = (100*baseline.acc),
-								 kappa = k)
-	}
-	return(stats)
 }
 
 

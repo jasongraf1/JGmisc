f836a4677ba2976a3f6195b8aa31528f7c5e6a08
Jason Grafmiller (Department of English Language and Applied Linguistics) <j.grafmiller@bham.ac.uk>
2019-07-07 11:23:16 +0100

    fix typo

--- R/notify.R
+++ R/notify.R
@@ -2,7 +2,7 @@ notify <- function(x = NULL){
   if (is.null(x)){
     msg <- 'CMD /C "ECHO The R process has finished running && PAUSE"'
     if (Sys.info()[1] == "Windows"){
-      windDialog(message = msg)
+      winDialog(message = msg)
     } else {
       system(msg, invisible = FALSE, wait = FALSE)
       }

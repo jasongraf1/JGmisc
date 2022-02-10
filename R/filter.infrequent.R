38324c7ed7b0f21bf7e7cebbb54cf9554d0b3175
Jason Grafmiller (English Language and Linguistics) <j.grafmiller@bham.ac.uk>
2022-02-10 14:01:22 +0000

    fix threshold, remove reliance on pipe

--- R/filter.infrequent.R
+++ R/filter.infrequent.R
@@ -1,11 +1,13 @@
 filter.infrequent <- function(words, threshold = 5, dummy = "OTHER") {
-	# code from WBRS for recoding infrequent factor levels (default is <= 5
-	# observations)
-	if (min(table(words)) > threshold){
-		return (words)
-	} else {
-		return (ifelse(words %in% levels(as.factor(words))[table(words) >= threshold],
-									 as.character(words), dummy) %>%
-							factor %>%
-							relevel(ref = dummy))
-	}}
+  # code from WBRS for recoding infrequent factor levels (default is <= 5
+  # observations)
+  # require(magrittr)
+  if (min(table(words)) > threshold){
+    return (words)
+  } else {
+    output <- ifelse(words %in% levels(as.factor(words))[table(words) > threshold],
+                     as.character(words), dummy)
+    output <- factor(output)
+    output <- relevel(output, ref = dummy)
+    return (output)
+  }}
